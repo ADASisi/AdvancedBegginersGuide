702e4505589a4450a0d8c8c41d306d492abb73a6
MartinR260 <127389329+MartinR260@users.noreply.github.com>
2023-03-10 13:08:47 +0200

    Add files via upload

---
+++ motion_sensor_led_piezobuzz.ino
@@ -0,0 +1,35 @@
+
+ 
+int led = 13;                // the pin that the LED is atteched to
+int sensor = 2;              // the pin that the sensor is atteched to
+int sound=12;
+int state = LOW;             // by default, no motion detected
+int val = 0;                 // variable to store the sensor status (value)
+
+void setup() {
+  pinMode(led, OUTPUT);      // initalize LED as an output
+  pinMode(sensor, INPUT);    // initialize sensor as an input
+  Serial.begin(9600); 
+}
+
+void loop(){
+  val = digitalRead(sensor);   // read sensor value
+  if (val == HIGH) {           // check if the sensor is HIGH
+    digitalWrite(led, HIGH);   // turn LED ON
+  int i=0;
+
+    if (state == LOW) {
+      Serial.println("Motion detected!"); 
+      tone(sound, 1500, 300);  
+      state = HIGH;       // update variable state to HIGH
+    }
+  }
+  if(val==LOW){
+      digitalWrite(led, LOW); // turn LED OFF
+      
+      if (state == HIGH){
+        Serial.println("Motion stopped!");
+        state = LOW;       // update variable state to LOW
+    }
+  }  
+}
